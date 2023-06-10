e9e8f78c901de2f24a469a1252a2696ab4bea471
Joaquin Gonzalez <joaquingonzalez008@gmail.com>
2023-06-10 14:18:47 -0300

    Puzzle 04

--- Year/2022/Problems/Problem04/ProblemFour.cs
+++ Year/2022/Problems/Problem04/ProblemFour.cs
@@ -4,16 +4,65 @@ using AOC.Helpers;
 using AOC.Interfaces;
 
 namespace AOC.Year._2022.Problems.Problem04 {
-	public class ProblemFour : IPuzzle, IReadTest<object> {
+	public class ProblemFour : IPuzzle, IReadTest<string[]> {
 		public int Year { get; set; }
 		public int Puzzle { get; set; }
 
 		public string SolvePartOne() {
-			throw new NotImplementedException();
+			var count = 0;
+
+			var lines = ReadTests();
+
+			foreach (var line in lines) {
+				var elfs = line.Split(',');
+				var elfOne = elfs[0];
+				var elfTwo = elfs[1];
+
+				var rangeElfOne = elfs[0].Split('-');
+				var initElfOne = int.Parse(rangeElfOne[0]);
+				var endElfOne = int.Parse(rangeElfOne[1]);
+
+				var rangeElfTwo = elfs[1].Split('-');
+				var initElfTwo = int.Parse(rangeElfTwo[0]);
+				var endElfTwo = int.Parse(rangeElfTwo[1]);
+
+				var rengeOneContainsRangeTwo = initElfTwo >= initElfOne && endElfTwo <= endElfOne;
+				var RangeTwoContainsrengeOne = initElfOne >= initElfTwo && endElfOne <= endElfTwo;
+
+				if (rengeOneContainsRangeTwo || RangeTwoContainsrengeOne) {
+					count++;
+				}
+
+			}
+
+			return count.ToString();
 		}
 
 		public string SolvePartTwo() {
-			throw new NotImplementedException();
+			var count = 0;
+
+			var lines = ReadTests();
+
+			foreach (var line in lines) {
+				var elfs = line.Split(',');
+				var elfOne = elfs[0];
+				var elfTwo = elfs[1];
+
+				var rangeElfOne = elfs[0].Split('-');
+				var initElfOne = int.Parse(rangeElfOne[0]);
+				var endElfOne = int.Parse(rangeElfOne[1]);
+
+				var rangeElfTwo = elfs[1].Split('-');
+				var initElfTwo = int.Parse(rangeElfTwo[0]);
+				var endElfTwo = int.Parse(rangeElfTwo[1]);
+
+				if (initElfOne <= endElfTwo && endElfOne >= initElfTwo) {
+					count++;
+				}
+
+			}
+
+			return count.ToString();
 		}
 
 		public void Solve() {
@@ -21,8 +70,8 @@ namespace AOC.Year._2022.Problems.Problem04 {
 			Console.WriteLine("Part Two: " + SolvePartTwo());
 		}
 
-		public object ReadTests() {
-			throw new NotImplementedException();
+		public string[] ReadTests() {
+			return FileHelper.GetFileText(Year, Puzzle);
 		}
 	}
 }
