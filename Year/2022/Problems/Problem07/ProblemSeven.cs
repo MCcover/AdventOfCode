a0f2b3b6ef7daa64966c0659b301e9f9994d56c9
Joaquin Gonzalez <joaquingonzalez008@gmail.com>
2023-06-12 14:06:10 -0300

    Puzzle 07

--- Year/2022/Problems/Problem07/ProblemSeven.cs
+++ Year/2022/Problems/Problem07/ProblemSeven.cs
@@ -1,19 +1,24 @@
-﻿
-
-using AOC.Helpers;
+﻿using AOC.Helpers;
 using AOC.Interfaces;
+using System.Text.RegularExpressions;
 
 namespace AOC.Year._2022.Problems.Problem07 {
-	public class ProblemSeven : IPuzzle, IReadTest<object> {
+
+	public class ProblemSeven : IPuzzle, IReadTest<List<int>> {
 		public int Year { get; set; }
 		public int Puzzle { get; set; }
 
 		public string SolvePartOne() {
-			throw new NotImplementedException();
+			return ReadTests().Where(size => size < 100000)
+							  .Sum()
+							  .ToString();
 		}
 
 		public string SolvePartTwo() {
-			throw new NotImplementedException();
+			var sizes = ReadTests();
+			return sizes.Where(size => size + (70000000 - sizes.Max()) >= 30000000)
+						.Min()
+						.ToString();
 		}
 
 		public void Solve() {
@@ -21,8 +26,28 @@ namespace AOC.Year._2022.Problems.Problem07 {
 			Console.WriteLine("Part Two: " + SolvePartTwo());
 		}
 
-		public object ReadTests() {
-			throw new NotImplementedException();
+		public List<int> ReadTests() {
+			var path = new Stack<string>();
+			var sizes = new Dictionary<string, int>();
+
+			var lines = FileHelper.GetFileAllLines(Year, Puzzle);
+
+			foreach (var line in lines) {
+				if (line == "$ cd ..") {
+					path.Pop();
+				} else if (line.StartsWith("$ cd")) {
+					var size = line.Split(" ")[2];
+					var newDirectory = string.Join("", path) + size;
+					path.Push(newDirectory);
+				} else if (Regex.Match(line, @"\d+").Success) {
+					var size = int.Parse(line.Split(" ")[0]);
+					foreach (var dir in path) {
+						sizes[dir] = sizes.GetValueOrDefault(dir) + size;
+					}
+				}
+			}
+
+			return sizes.Values.ToList();
 		}
 	}
 }
